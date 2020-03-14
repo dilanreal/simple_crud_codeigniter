f5af40bf306629535cb81df4ad2cd571d0e1f779
dilanreal <62044322+dilanreal@users.noreply.github.com>
2020-03-14 12:53:40 +0700

    Add files via upload

--- application/views/crud/view.php
+++ application/views/crud/view.php
@@ -1,16 +1,43 @@
 <div class="col-md-10">
+
+        <h1 class="my-4">Add
+          <small>Data </small>
+        </h1>
+
+  <font color="green"><?php echo $this->session->flashdata('pesan'); ?></font>
+  <?php echo form_open('crud/add',''); ?>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Name</label>
+    <input type="text" name="name"class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <?php echo form_error('name', '<div class="text-danger"><small>', '</small></div>');?>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Email</label>
+    <input type="email" name="email" class="form-control" id="exampleInputPassword1">
+    <?php echo form_error('email', '<div class="text-danger"><small>', '</small></div>');?>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Phone</label>
+    <input type="text" name="phone" class="form-control" id="exampleInputPassword1">
+    <?php echo form_error('phone', '<div class="text-danger"><small>', '</small></div>');?>
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+<?php echo form_close(); ?>
+</div>
+<div class="col-md-10">
 <h1 class="my-4">Views
           <small>Data </small>
         </h1>
 
-<?php echo anchor('crud/form_add', '+ Add data', 'class="btn btn-primary btn-sm"'); ?>
-<br><br>
 <font color="green"><?php echo $this->session->flashdata('pesan'); ?></font>
 
-<table class="table">
-  <thead>
+<table class="table"> 
+  <thead class="thead-dark>">
     <tr>
-      <th scope="col">#ID</th>
+      <th scope="col"></th>
       <th scope="col">Name</th>
       <th scope="col">Email</th>
       <th scope="col">Phone</th>
@@ -20,10 +47,11 @@
 
   <tbody>
     <?php 
-    $i=1;
+    $nomer=1;?>
+    <?php
     foreach ($tmp as $key) { ?>
     <tr>
-      <th scope="row"><?php echo $key->id; ?></th>
+      <td <?php echo $nomer; ?></td>
       <td><?php echo $key->name; ?></td>
       <td><?php echo $key->email; ?></td>
       <td><?php echo $key->phone; ?></td>
